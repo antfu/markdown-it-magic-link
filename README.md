2e6f0606716af868d2aeb0cbc6bb0833e1249311
Anthony Fu <anthonyfu117@hotmail.com>
2024-03-15 18:25:34 +0100

    chore: init

---
+++ README.md
@@ -0,0 +1,37 @@
+# markdown-it-magic-link
+
+[![npm version][npm-version-src]][npm-version-href]
+[![npm downloads][npm-downloads-src]][npm-downloads-href]
+[![bundle][bundle-src]][bundle-href]
+[![JSDocs][jsdocs-src]][jsdocs-href]
+[![License][license-src]][license-href]
+
+Makes links and `@mentions` in markdown beautiful and interactive.
+
+> [!IMPORTANT]
+> I made this plugin for my personal use - I don't have time to properly polish it yet. I'd suggest you don't use it at this moment, but if you do, be aware of the breakage or refactors might happen anytime.
+
+## Sponsors
+
+<p align="center">
+  <a href="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg">
+    <img src='https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg'/>
+  </a>
+</p>
+
+## License
+
+[MIT](./LICENSE) License © 2023-PRESENT [Anthony Fu](https://github.com/antfu)
+
+<!-- Badges -->
+
+[npm-version-src]: https://img.shields.io/npm/v/markdown-it-magic-link?style=flat&colorA=080f12&colorB=1fa669
+[npm-version-href]: https://npmjs.com/package/markdown-it-magic-link
+[npm-downloads-src]: https://img.shields.io/npm/dm/markdown-it-magic-link?style=flat&colorA=080f12&colorB=1fa669
+[npm-downloads-href]: https://npmjs.com/package/markdown-it-magic-link
+[bundle-src]: https://img.shields.io/bundlephobia/minzip/markdown-it-magic-link?style=flat&colorA=080f12&colorB=1fa669&label=minzip
+[bundle-href]: https://bundlephobia.com/result?p=markdown-it-magic-link
+[license-src]: https://img.shields.io/github/license/antfu/markdown-it-magic-link.svg?style=flat&colorA=080f12&colorB=1fa669
+[license-href]: https://github.com/antfu/markdown-it-magic-link/blob/main/LICENSE
+[jsdocs-src]: https://img.shields.io/badge/jsdocs-reference-080f12?style=flat&colorA=080f12&colorB=1fa669
+[jsdocs-href]: https://www.jsdocs.io/package/markdown-it-magic-link
---
+++ src/index.ts
@@ -0,0 +1,144 @@
+import type MarkdownIt from 'markdown-it'
+
+const reCapture = /^\{([^\{\}\n]*?)\}([^\[\]\{\}\(\)]|$)/i
+const reHtmlProtocol = /^https?:\/\//i
+const reGitHubScope = /^(https?:\/\/)?github\.com\/([^/]*?)(?:$|\/)/
+
+export interface ParsedMagicLink {
+  text?: string
+  link: string
+  type?: string
+  class?: string[]
+  imageUrl?: string
+}
+
+export interface ResolvedMagicLink extends Required<ParsedMagicLink> {}
+
+export interface MagicLinkHandler {
+  name: string
+  handler: (content: string) => ParsedMagicLink | void | false
+  postprocess?: (parsed: ResolvedMagicLink) => ResolvedMagicLink | void
+}
+
+export interface MarkdownItMagicLinkOptions {
+  handlers?: MagicLinkHandler[]
+}
+
+export function handlerLink(): MagicLinkHandler {
+  return {
+    name: 'link',
+    handler(content: string) {
+      const parts = content.split('|').map(i => i.trim())
+      let text = parts.length > 1 ? parts[0] : undefined
+      const url = parts.length > 1 ? parts[1] : parts[0]
+      const type = 'link'
+
+      if (!url.match(/^https?:\/\//))
+        return false
+
+      text ||= url.replace(reHtmlProtocol, '')
+
+      return {
+        text,
+        link: url,
+        type,
+        imageUrl: `https://favicon.yandex.net/favicon/${new URL(url || '').hostname}`,
+      }
+    },
+  }
+}
+
+export function handlerGitHubAt(): MagicLinkHandler {
+  return {
+    name: 'github-at',
+    handler(content: string) {
+      const parts = content.split('|').map(i => i.trim())
+      const text = parts.length > 1 ? parts[0] : undefined
+      const body = parts.length > 1 ? parts[1] : parts[0]
+
+      if (!body.match(/^@/))
+        return false
+
+      const login = body.slice(1)
+      return {
+        text: text || login,
+        link: `https://github.com/${login}`,
+        type: 'github-at',
+        imageUrl: `https://github.com/${login}.png`,
+      }
+    },
+    postprocess(parsed: ResolvedMagicLink) {
+      if (parsed.link.match(reGitHubScope) && parsed.type !== 'github-at')
+        parsed.imageUrl = `https://github.com/${parsed.link.match(reGitHubScope)![1]}.png`
+    },
+  }
+}
+
+export function parseMagicLink(content: string, handlers: MagicLinkHandler[]) {
+  for (const handler of handlers) {
+    const parsed = handler.handler(content)
+    if (parsed)
+      return parsed
+  }
+  return false
+}
+
+export default function MarkdownItMagicLink(md: MarkdownIt, options: MarkdownItMagicLinkOptions = {}) {
+  const {
+    handlers = [
+      handlerLink(),
+      handlerGitHubAt(),
+    ],
+  } = options
+
+  md.inline.ruler.before('text', 'magic-link', (state, silent) => {
+    if (state.src.charCodeAt(state.pos) !== '{'.charCodeAt(0))
+      return false
+
+    const starts = state.src.slice(state.pos)
+    const match = starts.match(reCapture)
+    if (!match)
+      return false
+
+    const fullMatch = match[0]
+    const tailingChar = match[2]
+
+    const parsed = parseMagicLink(match[1], handlers)
+    if (!parsed)
+      return false
+
+    let resolved = {
+      class: [],
+      ...parsed,
+    } as ResolvedMagicLink
+
+    resolved.link = state.md.normalizeLink(parsed.link)
+    resolved.class.push('markdown-magic-link', `markdown-magic-link-${parsed.type}`)
+    resolved.text ||= resolved.link.replace(reHtmlProtocol, '')
+    resolved.imageUrl ||= `https://favicon.yandex.net/favicon/${new URL(resolved.link || '').hostname}`
+
+    for (const handler of handlers)
+      resolved = handler.postprocess?.(resolved) || resolved
+
+    if (!silent) {
+      const token_o = state.push('link_open', 'a', 1)
+      token_o.attrs = [
+        ['href', resolved.link],
+        ['class', resolved.class.join(' ')],
+      ]
+      token_o.info = 'auto'
+
+      const token_i = state.push('html_inline', '', 0)
+      token_i.content = `<span class="markdown-magic-link-image" style="background-image: url('${resolved.imageUrl}');"></span>`
+
+      const token_t = state.push('text', '', 0)
+      token_t.content = resolved.text
+
+      const token_c = state.push('link_close', 'a', -1)
+      token_c.info = 'auto'
+    }
+
+    state.pos += fullMatch.length - tailingChar.length
+    return true
+  })
+}
